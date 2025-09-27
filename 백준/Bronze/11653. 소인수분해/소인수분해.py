d2818b06fb59c55b8f58a2fea3659ccc2fd1b029
efwCherry99 <131515430+StHy47@users.noreply.github.com>
2025-09-27 18:54:50 +0900

    [Bronze I] Title: 소인수분해, Time: 2188 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n == 1:
+    print(" ")
+else:
+    a = 2
+    while n >= a:
+
+        if n //a  == n/a:
+            n = n/a
+            print(a)
+        else:
+            a += 1
