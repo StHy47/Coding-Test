d4601e5bd1f44c03c31580ea31af7c14ffe6b33a
efwCherry99 <131515430+StHy47@users.noreply.github.com>
2025-09-29 17:52:14 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
@@ -0,0 +1,15 @@
+n = int(input())
+cnt_0 = 0
+cnt_1 = 0
+
+for _ in range(n):
+    a = int(input())
+    if a == 0:
+        cnt_0 += 1
+    else:
+        cnt_1 += 1
+
+if cnt_0 > cnt_1:
+    print('Junhee is not cute!')
+else:
+    print('Junhee is cute!')
