ec21a48def5077a3f54af959b7e00f7e3be09344
efwCherry99 <131515430+StHy47@users.noreply.github.com>
2025-11-22 13:51:38 +0900

    fix : 풀이 추가
    Signed-off-by: efwCherry99 <131515430+StHy47@users.noreply.github.com>

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,3 +1,4 @@
+## 문제 풀이 아이디어 : dp에 계산한 값을 추가로 더해주며 누적합과 현재 값 비교
 n = int(input())
 num = list(map(int, input().split()))
 
@@ -10,9 +11,27 @@ for i in range(1, n):
 print(max(dp))
     
 
+#### 처음 풀이 - 시간초과 : for 문을 이중으로 써서 시간 복잡도가 큼
+'''
+n = int(input())
+num = list(map(int, input().split()))
+
+dp = {} ## 선택 갯수 i - dp[i] = [#선택 가능한 값 리스트]
+
+for i in range(1,n+1):    #선택할 수 갯수
+    dp[i] = 0
+    for j in range(n):
+        n_sum = sum(num[j:j+i])
+        if n_sum > dp[i]:
+            dp[i] = n_sum
+            
+print(max(dp.values))
+
+'''
 
 
         
         
     
+
     
